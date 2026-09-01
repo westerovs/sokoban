d95bae05eec9feac306e2726eec0557ecde83271
westerovs@mail.ru <westerovs@mail.ru>
2026-09-01 14:35:17 +0300

    codex: разделить уровни по файлам локаций - генерировать отдельный JSON для каждой локации - заменить монолитный levels.json компактным индексом - адаптировать редактор и сборщик ассетов

---
+++ src/game/gameConfig/levels/levels.js
@@ -0,0 +1,42 @@
+import antarctica1Location from './generated/antarctica-1.json'
+import antarctica2Location from './generated/antarctica-2.json'
+import antarctica3Location from './generated/antarctica-3.json'
+import factory1Location from './generated/factory-1.json'
+import factory2Location from './generated/factory-2.json'
+import factory3Location from './generated/factory-3.json'
+import forest1Location from './generated/forest-1.json'
+import forest2Location from './generated/forest-2.json'
+import forest3Location from './generated/forest-3.json'
+import garden1Location from './generated/garden-1.json'
+import garden2Location from './generated/garden-2.json'
+import garden3Location from './generated/garden-3.json'
+import garden4Location from './generated/garden-4.json'
+import mine1Location from './generated/mine-1.json'
+import mine2Location from './generated/mine-2.json'
+import mine3Location from './generated/mine-3.json'
+
+/** Автоматически созданный индекс игровых локаций. Не редактировать вручную. */
+const levels = {
+  locations: [
+    garden1Location,
+    antarctica1Location,
+    forest1Location,
+    garden2Location,
+    mine1Location,
+    factory1Location,
+    antarctica2Location,
+    forest2Location,
+    garden3Location,
+    mine2Location,
+    factory2Location,
+    antarctica3Location,
+    forest3Location,
+    garden4Location,
+    mine3Location,
+    factory3Location,
+  ],
+}
+
+export {
+  levels,
+}
